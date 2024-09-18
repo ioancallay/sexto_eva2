2164ff0ea4fd30d61ec52a93c1998bc50f914797
ioancallay <ivanar71@uniandes.edu.ec>
2024-09-18 11:35:50 -0500

    servicio niños

--- 02berry/src/app/Components/ninios/nuevoninio/nuevoninio.component.ts
+++ 02berry/src/app/Components/ninios/nuevoninio/nuevoninio.component.ts
@@ -1,6 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { NiniosService } from 'src/app/Services/ninios.service';
 
 @Component({
   selector: 'app-nuevoninio',
@@ -10,6 +12,19 @@ import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angula
   styleUrl: './nuevoninio.component.scss'
 })
 export class NuevoninioComponent implements OnInit {
+  idNinio: number = 0;
+  btn_save: string = 'Crear cuidador';
+  btn_confirm: string = 'Crear cuidador';
+  mensaje: string = 'Desea crear el cuidador';
+  titulo: string = 'Nuevo cuidador';
+  frm_ninio: FormGroup;
+
+  constructor(
+    private ServicioNinios: NiniosService,
+    private rutas: ActivatedRoute,
+    private navegacion: Router
+  ) {}
+
   ngOnInit(): void {
     this.frm_ninio = new FormGroup({
       Nombre: new FormControl('', Validators.required),
@@ -18,10 +33,6 @@ export class NuevoninioComponent implements OnInit {
       Alergias: new FormControl('', Validators.required)
     });
   }
-
-  title: string = 'Nuevo Niño';
-  btn_save: string = 'Guardar';
-  frm_ninio: FormGroup;
 
   grabar() {}
 }
