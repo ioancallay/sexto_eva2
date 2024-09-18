58b0c7bd9c78a3549f85b3a9c213dbaa0f7cb1ed
ioancallay <ivanar71@uniandes.edu.ec>
2024-09-18 07:18:25 -0500

    servicio niños

--- 02berry/src/app/Services/ninios.service.ts
+++ 02berry/src/app/Services/ninios.service.ts
@@ -1,9 +1,53 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { INinios } from '../Interfaces/ininios';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NiniosService {
+  // ninio_id INT AUTO_INCREMENT PRIMARY KEY,
+  // nombre VARCHAR(50) NOT NULL,
+  // apellido VARCHAR(50) NOT NULL,
+  // fecha_nacimiento DATE NOT NULL,
+  // alergias TEXT
 
-  constructor() { }
+  apiURL = 'http://localhost/sexto_eva2/01mvc/controllers/ninios.controller.php?op=';
+  constructor(private http: HttpClient) {}
+
+  todos(): Observable<INinios[]> {
+    return this.http.get<INinios[]>(this.apiURL + 'todos');
+  }
+
+  uno(idNinio: number): Observable<INinios> {
+    const formData = new FormData();
+    formData.append('idNinio', idNinio.toString());
+    return this.http.get<INinios>(this.apiURL + 'uno' + formData);
+  }
+
+  insertar(ninio: INinios): Observable<string> {
+    const formData = new FormData();
+    formData.append('nombre', ninio.nombre);
+    formData.append('apellido', ninio.apellido);
+    formData.append('fecha_nacimiento', ninio.fecha_nacimiento.toString());
+    formData.append('alergias', ninio.alergias);
+    return this.http.post<string>(this.apiURL + 'insertar', formData);
+  }
+
+  actualizar(ninio: INinios): Observable<string> {
+    const formData = new FormData();
+    formData.append('idNinio', ninio.ninio_id.toString());
+    formData.append('nombre', ninio.nombre);
+    formData.append('apellido', ninio.apellido);
+    formData.append('fecha_nacimiento', ninio.fecha_nacimiento.toString());
+    formData.append('alergias', ninio.alergias);
+    return this.http.post<string>(this.apiURL + 'actualizar', formData);
+  }
+
+  eliminar(idNinio: number): Observable<number> {
+    const formData = new FormData();
+    formData.append('idNinio', idNinio.toString());
+    return this.http.post<number>(this.apiURL + 'eliminar', formData);
+  }
 }
