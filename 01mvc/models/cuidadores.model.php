1107d0f0b176bfad24159108b2a386fd8a997014
ioancallay <ivanar71@uniandes.edu.ec>
2024-09-18 05:45:09 -0500

    agregar modelo cuidadores

---
+++ 01mvc/models/cuidadores.model.php
@@ -0,0 +1,87 @@
+<?php
+
+require_once '../config/conexion.php';
+
+class CuidadoresModel
+{
+
+    // cuidador_id INT AUTO_INCREMENT PRIMARY KEY,
+    // nombre VARCHAR(50) NOT NULL,
+    // especialidad VARCHAR(50) NOT NULL,
+    // telefono VARCHAR(20),
+    // email VARCHAR(100) UNIQUE
+
+    public function todos()
+    {
+        $con = new ClaseConexion();
+        $con = $con->ProcedimientoConectar();
+        $cadena = "SELECT * FROM cuidadores";
+        $datos = mysqli_query($con, $cadena);
+        $con->close();
+        return $datos;
+    }
+
+    public function uno($idCuidador)
+    {
+        $con = new ClaseConexion();
+        $con = $con->ProcedimientoConectar();
+        $cadena = "SELECT * FROM cuidadores WHERE cuidador_id = $idCuidador";
+        $datos = mysqli_query($con, $cadena);
+        $con->close();
+        return $datos;
+    }
+
+    public function insertar($nombre, $especialidad, $telefono, $email)
+    {
+        try {
+            $con = new ClaseConexion();
+            $con = $con->ProcedimientoConectar();
+            $cadena = "INSERT INTO cuidadores (nombre, especialidad, telefono, email) VALUES ('$nombre', '$especialidad', '$telefono', '$email')";
+            if (mysqli_query($con, $cadena)) {
+                return $con->insert_id;
+            } else {
+                return $con->error;
+            }
+        } catch (Exception $e) {
+            return $e->getMessage();
+        } finally {
+            $con->close();
+        }
+    }
+
+    public function actualizar($idCuidador, $nombre, $especialidad, $telefono, $email)
+    {
+        try {
+            $con = new ClaseConexion();
+            $con = $con->ProcedimientoConectar();
+            $cadena = "UPDATE cuidadores SET nombre='$nombre', especialidad='$especialidad', telefono='$telefono', email='$email' WHERE cuidador_id=$idCuidador";
+            if (mysqli_query($con, $cadena)) {
+                return $con->insert_id;
+            } else {
+                return $idCuidador;
+            }
+        } catch (Exception $e) {
+            return $e->getMessage();
+        } finally {
+            $con->close();
+        }
+    }
+
+    public function eliminar($idCuidador)
+    {
+        try {
+            $con = new ClaseConexion();
+            $con = $con->ProcedimientoConectar();
+            $cadena = "DELETE FROM cuidadores WHERE cuidador_id=$idCuidador";
+            if (mysqli_query($con, $cadena)) {
+                return $idCuidador;
+            } else {
+                return false;
+            }
+        } catch (Exception $e) {
+            return $e->getMessage();
+        } finally {
+            $con->close();
+        }
+    }
+}
