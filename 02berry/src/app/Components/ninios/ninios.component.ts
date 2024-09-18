78589ab5ce071790f4f8260696a0123c7ad55791
ioancallay <ivanar71@uniandes.edu.ec>
2024-09-18 08:43:44 -0500

    agregar imports

--- 02berry/src/app/Components/ninios/ninios.component.ts
+++ 02berry/src/app/Components/ninios/ninios.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { INinios } from 'src/app/Interfaces/ininios';
+import { NiniosService } from 'src/app/Services/ninios.service';
+import { SharedModule } from 'src/app/theme/shared/shared.module';
 
 @Component({
   selector: 'app-ninios',
   standalone: true,
-  imports: [],
+  imports: [SharedModule, RouterLink],
   templateUrl: './ninios.component.html',
   styleUrl: './ninios.component.scss'
 })
-export class NiniosComponent {
+export class NiniosComponent implements OnInit {
+  constructor(private ServicioNinios: NiniosService) {}
+  ngOnInit(): void {
+    this.ServicioNinios.todos().subscribe((data) => (this.listaNinios = data));
+  }
+  title: string = 'Ninios';
+  listaNinios: INinios[] = [];
 
+  eliminar(ninio_id: number) {}
 }
